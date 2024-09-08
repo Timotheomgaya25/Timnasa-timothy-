89217a22760419f4273879111c30d6c008d4b41e
Yassin994 <aidenyassin115@gmail.com>
2024-09-08 17:04:37 +0300

    Update uptime.js

--- commandes/uptime.js
+++ commandes/uptime.js
@@ -28,7 +28,7 @@ zokou({ nomCom: 'uptime',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
 
-                 await repondre(`*_uptime of boniphace md bot is: ${runtime(process.uptime())}_*`) 
+                 await repondre(`*_uptime of YESSER md bot is: ${runtime(process.uptime())}_*`) 
 
    
 
@@ -57,7 +57,7 @@ let linkkk = `https://api.screenshotmachine.com/?key=c04d3a&url=${encodeURICompo
 
 let res = await getBuffer(linkkk);
 
-await zk.sendMessage(dest, { image: res, caption: 'powered  𝒃𝒚 * BONIPHACE TECH*'}, { quoted: ms });
+await zk.sendMessage(dest, { image: res, caption: 'powered  𝒃𝒚 * YESSER TECH*'}, { quoted: ms });
 
 
 }
