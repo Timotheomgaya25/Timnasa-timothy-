e7656459f745d346824372ae332578bb9ea6aa82
Yassin994 <aidenyassin115@gmail.com>
2024-09-08 18:02:53 +0300

    Update igdl-fb-tk .js

--- commandes/igdl-fb-tk .js
+++ commandes/igdl-fb-tk .js
@@ -113,7 +113,7 @@ async (dest, zk, commandeOptions) => {
         Lien: ${result.url}
       `;
        zk.sendMessage(dest,{image : { url : result.thumbnail}, caption : caption},{quoted : ms}) ;
-       zk.sendMessage(dest, { video: { url: result.sd  }, caption: 'facebook video downloader powered by *BONIPHACE-MD*' }, { quoted: ms });
+       zk.sendMessage(dest, { video: { url: result.sd  }, caption: 'facebook video downloader powered by *YESSER-MD*' }, { quoted: ms });
       
     })
     .catch((error) => {console.log("Error:", error)
