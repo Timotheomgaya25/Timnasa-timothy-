2907008b4bfdf27e2bacc6370231f990437f5516
Yassin994 <aidenyassin115@gmail.com>
2024-09-08 16:20:59 +0300

    Update zokou.js

--- framework/zokou.js
+++ framework/zokou.js
@@ -6,7 +6,7 @@ function zokou(obj, fonctions) {
         infoComs.categorie = "General";
     }
     if (!obj.reaction) {
-        infoComs.reaction = "🥰";
+        infoComs.reaction = "✍️";
     }
     infoComs.fonction = fonctions;
     cm.push(infoComs);
