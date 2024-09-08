a63b436dc1a029003ea1106ed00c80a8d664ac2f
Yassin994 <aidenyassin115@gmail.com>
2024-09-08 16:51:55 +0300

    Update chanel.js

--- commandes/chanel.js
+++ commandes/chanel.js
@@ -2,10 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 
-zokou({ nomCom: "channel", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "channel", reaction: "😁", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Salut je m\'appelle *BONIPHACE-MD* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
-    let d = ' developpé par *Boniphace Tech💎*';
+    let z = 'Salut je m\'appelle *YESSER-MD* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
+    let d = ' developpé par *yesser boy😎*';
     let varmess = z + d;
     var lien = 'https://whatsapp.com/channel/0029VaiMm7d4yltT51HS1T1G';  // Remplacez cet URL par le lien que vous souhaitez envoyer
     await zk.sendMessage(dest, { text: varmess + "\n" + lien });
