4351943ecd887d1f80e003be9301b9264783b43f
Yassin994 <aidenyassin115@gmail.com>
2024-09-08 22:37:08 +0300

    Update anti-delete.js

--- commandes/anti-delete.js
+++ commandes/anti-delete.js
@@ -7,7 +7,7 @@ let antiDeleteActive = false; // Variable pour stocker l'état de la commande an
 zokou({
   nomCom: "anti-delete",
   categorie: "General",
-  reaction: "🥺"
+  reaction: "😑"
 }, async (origineMessage, zk, commandeOptions) => {
   const { ms, arg } = commandeOptions;
 
@@ -61,7 +61,7 @@ zokou({
       }
 
       const senderId = msg.key.participant.split('@')[0];
-      const caption = ` Anti-delete-message by Boniphace Tech\nMessage de @${senderId}`;
+      const caption = ` Anti-delete-message by yesser Tech\nMessage de @${senderId}`;
       const imageCaption = { image: { url: './media/deleted-message.jpg' }, caption, mentions: [msg.key.participant] };
 
       await zk.sendMessage(idBot, imageCaption);
